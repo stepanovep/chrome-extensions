51b629a81af153697d6d8a74a4293e01d678a0bf
stepanovep <stepanovep@gmail.com>
2020-01-04 03:47:37 +0300

    added script - shortcut to like a youtube video by pressing 'Enter'

---
+++ tampermonkey/youtube/like_shortcut.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Youtube like button shortcut
+// @version      0.1
+// @description  Provides a shortcut to like a youtube video by pressing `Enter`
+// @author       stepanovep
+// @include      https://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+let like_button;
+
+function init() {
+    let buttons = document.querySelectorAll('#top-level-buttons > ytd-toggle-button-renderer');
+    if (buttons.length > 0) {
+        like_button = buttons[0];
+        console.log('like button found');
+        return;
+    }
+
+    console.log('like button not found');
+}
+
+const observer = new MutationObserver(init);
+observer.observe(document.documentElement, {childList: true, subtree: true});
+
+init();
+
+document.addEventListener('keydown', function(event) {
+    console.log(event.code);
+    if (event.code === 'Enter' && like_button) {
+        like_button.click();
+        console.log('like button clicked');
+    }
+});
