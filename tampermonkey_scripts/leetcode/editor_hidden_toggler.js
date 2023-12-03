8ab433691dd99644e34feaae9f9ce83d5abcc14a
stepanovep <stepanovep@gmail.com>
2023-12-03 15:56:19 +0600

    editor hider script

---
+++ tampermonkey_scripts/leetcode/editor_hidden_toggler.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         leetcode editor hidden toggler
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  press `rightAlt` to hide/unhide editor content
+// @author       stepanovep
+// @match        https://leetcode.com/problems/*
+// @grant        none
+// ==/UserScript==
+
+
+var editor;
+var editorElemNotFoundYet = true;
+
+function discoverEditorElem() {
+    editor = document.querySelector('#editor');
+    if (editorElemNotFoundYet && editor) {
+        editorElemNotFoundYet = false;
+        editor.hidden = true;
+    }
+}
+
+const observer = new MutationObserver(discoverEditorElem);
+observer.observe(document.documentElement, {childList: true, subtree: true});
+
+discoverEditorElem();
+
+document.addEventListener('keydown', function(event) {
+    if (editor && event.altKey && event.code === 'KeyH') {
+        console.log('Toggling editor hidden property');
+        editor.hidden = !editor.hidden;
+    }
+});
